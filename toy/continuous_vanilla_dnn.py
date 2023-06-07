f34775877a4ed0796add91e6946c466ecd3eb1d7
Porter Jenkins <porterjenkins@gmail.com>
2023-06-07 16:37:16 -0600

    train and plot results for vanilla DNN

--- toy/continuous_vanilla_dnn.py
+++ toy/continuous_vanilla_dnn.py
@@ -5,11 +5,13 @@ import torch
 from torch import nn
 from tqdm import tqdm
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 
 from utils import get_yaml
 from torch.utils.data import DataLoader, TensorDataset
 from models.regressors import RegressionNN
+from models.model_utils import get_1d_plot
 from toy.gen_data import generate_gaussian_data
 
 def train_regression_nn(train_loader, model, criterion, optimizer, device):
@@ -63,12 +65,20 @@ def main(config: dict):
         torch.Tensor(X_train.reshape(-1, 1)),
         torch.Tensor(y_train.reshape(-1, 1))
     )
+    val_dataset = TensorDataset(
+        torch.Tensor(X_val.reshape(-1, 1)),
+        torch.Tensor(y_val.reshape(-1, 1))
+    )
+
     test_dataset = TensorDataset(
         torch.Tensor(X_test.reshape(-1, 1)),
         torch.Tensor(y_test.reshape(-1, 1))
     )
+
+
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     # Check for CUDA availability and set the device accordingly
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -76,21 +86,33 @@ def main(config: dict):
     # Instantiate and train the network
     model = RegressionNN().to(device)
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
     # Train the network
-    num_epochs = 1000
+    num_epochs = 100
     progress_bar = tqdm(range(num_epochs), desc="Training", unit="epoch")
+    trn_losses = []
+    val_losses = []
     for epoch in progress_bar:
         train_loss = train_regression_nn(train_loader, model, criterion, optimizer, device)
         val_loss = evaluate_regression_nn(val_loader, model, criterion, device)
 
         progress_bar.set_postfix({"Train Loss": f"{train_loss:.4f}", "Val Loss": f"{val_loss:.4f}"})
+        trn_losses.append(train_loss)
+        val_losses.append(val_loss)
 
 
 
+    test_loss = evaluate_regression_nn(test_loader, model, criterion, device)
+    print(test_loss)
+
+    plt.plot(np.arange(num_epochs), trn_losses, label="TRAIN")
+    plt.plot(np.arange(num_epochs), val_losses, label="VAL")
+    plt.legend()
+    plt.show()
 
 
+    get_1d_plot(X_test, y_test, model)
 
 
 

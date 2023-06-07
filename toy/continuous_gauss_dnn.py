7ae01c93a43e306e995843bbd247ebfa2fb79ec5
Porter Jenkins <porterjenkins@gmail.com>
2023-06-07 17:04:15 -0600

    setup gaussian DNN experiment

--- toy/continuous_gauss_dnn.py
+++ toy/continuous_gauss_dnn.py
@@ -1,1 +1,138 @@
+import numpy as np
+import argparse
+import os
+import torch
+from torch import nn
+from tqdm import tqdm
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+
+from utils import get_yaml
 from torch.utils.data import DataLoader, TensorDataset
+from models.regressors import GaussianDNN
+from models.model_utils import get_1d_plot
+from toy.gen_data import generate_gaussian_data
+
+
+def train_gaussian_dnn(train_loader, model, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for inputs, targets in train_loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        mean, logvar = model(inputs)
+
+        loss = 0.5 * (torch.exp(-logvar) * (targets - mean) ** 2 + logvar).mean()
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * inputs.size(0)
+    return running_loss / len(train_loader.dataset)
+
+
+def evaluate_gaussian_dnn(val_loader, model, device):
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for inputs, targets in val_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            mean, logvar = model(inputs)
+
+            loss = 0.5 * (torch.exp(-logvar) * (targets - mean) ** 2 + logvar).mean()
+            running_loss += loss.item() * inputs.size(0)
+    return running_loss / len(val_loader.dataset)
+
+def main(config: dict):
+
+    X_train = np.loadtxt(
+        fname=os.path.join(config['dataset']["dir"], config['dataset']["name"] + "_x_train.txt")
+    )
+    X_val = np.loadtxt(
+        fname=os.path.join(config['dataset']["dir"], config['dataset']["name"] + "_x_val.txt")
+    )
+    X_test = np.loadtxt(
+        fname=os.path.join(config['dataset']["dir"], config['dataset']["name"] + "_x_test.txt")
+    )
+    y_train = np.loadtxt(
+        fname=os.path.join(config['dataset']["dir"], config['dataset']["name"] + "_y_train.txt")
+    )
+    y_val = np.loadtxt(
+        fname=os.path.join(config['dataset']["dir"], config['dataset']["name"] + "_y_val.txt")
+    )
+    y_test = np.loadtxt(
+        fname=os.path.join(config['dataset']["dir"], config['dataset']["name"] + "_y_test.txt")
+    )
+
+
+    train_dataset = TensorDataset(
+        torch.Tensor(X_train.reshape(-1, 1)),
+        torch.Tensor(y_train.reshape(-1, 1))
+    )
+    val_dataset = TensorDataset(
+        torch.Tensor(X_val.reshape(-1, 1)),
+        torch.Tensor(y_val.reshape(-1, 1))
+    )
+
+    test_dataset = TensorDataset(
+        torch.Tensor(X_test.reshape(-1, 1)),
+        torch.Tensor(y_test.reshape(-1, 1))
+    )
+
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    # Check for CUDA availability and set the device accordingly
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Instantiate and train the network
+    model = GaussianDNN().to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    # Train the network
+    num_epochs = 500
+    progress_bar = tqdm(range(num_epochs), desc="Training", unit="epoch")
+    trn_losses = []
+    val_losses = []
+    for epoch in progress_bar:
+        train_loss = train_gaussian_dnn(train_loader, model, optimizer, device)
+        val_loss = evaluate_gaussian_dnn(val_loader, model, device)
+
+        progress_bar.set_postfix({"Train Loss": f"{train_loss:.4f}", "Val Loss": f"{val_loss:.4f}"})
+        trn_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+
+
+    test_loss = evaluate_gaussian_dnn(test_loader, model, device)
+    print("Test MSE: {:.4f}".format(test_loss))
+
+    plt.plot(np.arange(num_epochs), trn_losses, label="TRAIN")
+    plt.plot(np.arange(num_epochs), val_losses, label="VAL")
+    plt.legend()
+    plt.show()
+
+
+    get_1d_plot(X_test, y_test, model)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--trn-cfg', type=str, default="./toy_exp_train_config.yaml", help='Path to data config file')
+    args = parser.parse_args()
+
+    cfg = get_yaml(args.trn_cfg)
+
+    main(config=cfg)

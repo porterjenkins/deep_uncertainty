6ec3f1e79a578f8daaf0c2b4359c81ed584324b9
Porter Jenkins <porterjenkins@gmail.com>
2023-06-07 16:37:16 -0600

    train and plot results for vanilla DNN

--- models/model_utils.py
+++ models/model_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import matplotlib.pyplot as plt
 
 def get_binom_p(mu: np.ndarray, n: np.ndarray):
     """
@@ -18,3 +20,26 @@ def get_binom_n(mu: np.array, sig2: np.array):
     """
 
     return mu/(1-(sig2/mu))
+
+
+
+def get_1d_plot(X, y, model):
+
+    x_for_pred = np.sort(X)
+    x_for_pred = torch.Tensor(x_for_pred.reshape(-1, 1))
+
+    model.eval()
+
+    with torch.no_grad():
+        y_predicted_tensor = model(x_for_pred)
+        y_predicted = y_predicted_tensor.squeeze().numpy()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(X, y, alpha=0.5, label="Generated Data")
+    plt.plot(x_for_pred, y_predicted, label="Predicted", color='r', linestyle='-', linewidth=2)
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Simple Regression')
+    plt.legend()
+    plt.show()

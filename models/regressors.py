4e8c0a8fdd376615600fbf5c676b9a4636b46fe2
Porter Jenkins <porterjenkins@gmail.com>
2023-06-07 17:04:15 -0600

    setup gaussian DNN experiment

--- models/regressors.py
+++ models/regressors.py
@@ -14,3 +14,19 @@ class RegressionNN(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+
+class GaussianDNN(nn.Module):
+    def __init__(self):
+        super(GaussianDNN, self).__init__()
+        self.fc1 = nn.Linear(1, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.mean_output = nn.Linear(32, 1)
+        self.logvar_output = nn.Linear(32, 1)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        mean = self.mean_output(x)
+        logvar = self.logvar_output(x)
+        return mean, logvar

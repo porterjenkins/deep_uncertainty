a16f670480ba4e61027486e8819400a785320c26
Porter Jenkins <porterjenkins@gmail.com>
2023-06-21 10:02:19 -0600

    poisson regression

--- evaluation/evals.py
+++ evaluation/evals.py
@@ -24,3 +24,26 @@ def evaluate_model_mse(val_loader, model, device):
 
     return mse
 
+def evaluate_model_criterion(val_loader, model, criterion, device):
+    model.eval()
+    preds_all = []
+    targets_all = []
+    with torch.no_grad():
+        for inputs, targets in val_loader:
+            pred = model(inputs.to(device))
+            if isinstance(pred, tuple):
+                mean, logvar = pred
+            else:
+                mean = pred
+
+            preds_all.append(mean.cpu())
+            targets_all.append(targets)
+
+    preds_all = torch.cat(preds_all)
+    targets_all = torch.cat(targets_all)
+
+    loss = criterion(preds_all, targets_all)
+
+    return loss
+
+

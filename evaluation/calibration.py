94c2f15373ac8b1b0e27f01821d7230fb6207b67
Spencer Young <spencer.young@deliciousai.com>
2023-06-21 11:15:45 -0600

    Added shading for credible interval plot, support for discrete/cont rvs

--- evaluation/calibration.py
+++ evaluation/calibration.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import Optional, Union, Tuple
-from scipy.stats import norm, truncnorm, binom
+from typing import Optional, Tuple
+from scipy.stats import norm, truncnorm, binom, poisson
 from scipy.integrate import quad
 import warnings
-
 
 def get_bayes_credible_interval(rv, p: float) -> Tuple[float, float]:
     """Given a random variable `rv` and a value `p`, return the (centered) Bayes Credible Interval in which the random variable falls with probability `p`.
@@ -14,7 +13,7 @@ def get_bayes_credible_interval(rv, p: float) -> Tuple[float, float]:
         p (float): The probability of the credible interval to construct.
 
     Returns:
-        confidence_interval_min (float), confidence_interval_max (float): The lower and upper bounds of the credible interval
+        credible_interval_min (float), credible_interval_max (float): The lower and upper bounds of the credible interval.
     """
     p_low = (1 - p) / 2
     p_high = p_low + p
@@ -22,14 +21,41 @@ def get_bayes_credible_interval(rv, p: float) -> Tuple[float, float]:
 
     return credible_interval_min, credible_interval_max
 
-def display_bayes_credible_interval(rv, p: float):
-    interval_min, interval_max = get_bayes_credible_interval(rv, p)
-    support = np.linspace(rv.ppf(0.001), rv.ppf(0.999), 100)
-    plt.plot(support, rv.pdf(support), color='black')
-    plt.ylim(bottom=0)
-    plt.vlines(x=[interval_min, interval_max], ymin=[0, 0], ymax=[rv.pdf(interval_min), rv.pdf(interval_max)], colors=['red', 'red'])
-    plt.show()
+def display_bayes_credible_interval(rv, p: float, ax: Optional[plt.Axes] = None, show=True) -> Tuple[float, float]:
+    """Given a univariate random variable and a value `p`, overlay the pmf/pdf with the centered p% Bayes Credible Interval.
     
+    Args:
+        rv: The (univariate) random variable to construct a credible interval for.
+        p (float): The probability of the credible interval to construct.
+
+    Returns:
+        credible_interval_min (float), credible_interval_max (float): The lower and upper bounds of the credible interval.
+    """
+    credible_interval_min, credible_interval_max = get_bayes_credible_interval(rv, p)
+    ax = plt.subplots(1, 1)[1] if ax is None else ax
+
+    if hasattr(rv, "pmf"):
+        support = np.arange(rv.ppf(0.001), rv.ppf(0.999))
+        ax.plot(support, rv.pmf(support), color='black', linestyle='-', marker='o')
+        ax.set_ylim(bottom=0)
+        ax.set_xlabel("Support")
+        ax.set_ylabel("Probability")
+        ax.set_title(f"{p * 100}% credible interval")
+        ax.fill_between(x=support, y1=0, y2=rv.pmf(support), where=(support >= credible_interval_min) & (support <= credible_interval_max), color='green', alpha=0.3)
+        
+    elif hasattr(rv, "pdf"):
+        support = np.linspace(rv.ppf(0.001), rv.ppf(0.999), 300)
+        ax.plot(support, rv.pdf(support), color='black')
+        ax.set_ylim(bottom=0)
+        ax.set_xlabel("Support")
+        ax.set_ylabel("Density")
+        ax.set_title(f"{p * 100}% credible interval")
+        ax.fill_between(x=support, y1=0, y2=rv.pdf(support), where=(support >= credible_interval_min) & (support <= credible_interval_max), color='green', alpha=0.3)
+    
+    if show:
+        plt.show()
+
+    return credible_interval_min, credible_interval_max
 
 def get_pct_of_targets_in_pred_credible_interval(y_true: np.ndarray, posterior_predictive_distribution, p: float = 0.95) -> float:
     """Return the percentage of ground truth targets that are within the p% credible interval of a probabilistic regression model.
@@ -103,10 +129,13 @@ if __name__ == "__main__":
     y_true = 3 * x
 
     y_pred = y_true + np.random.randn(1000)
-    sigma_pred = 3 * np.ones_like(x)    # We have nearly perfect calibration here since our model knows its uncertainty is 1.
+    sigma_pred = np.ones_like(x)    # We have nearly perfect calibration here since our model knows its uncertainty is 1.
     posterior_predictive_distribution = norm(y_pred, sigma_pred)
 
     plot_regression_calibration_curve(y_true, posterior_predictive_distribution, num_bins=9)
-    print(compute_average_calibration_score(y_true, posterior_predictive_distribution))    # Should be close to 1.
+    print(f"Average Calibration: {compute_average_calibration_score(y_true, posterior_predictive_distribution)}")    # Should be close to 1.
 
-    display_bayes_credible_interval(binom(3, 0.5), p=0.9)
+    display_bayes_credible_interval(norm(7, 3), p=0.95)
+    display_bayes_credible_interval(truncnorm(loc=5, scale=2.3, a=-2, b=2), p=0.95)
+    display_bayes_credible_interval(binom(n=15, p=0.7), p=0.95)
+    display_bayes_credible_interval(poisson(mu=17), p=0.95)

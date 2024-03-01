2b1cb2a9ed4082d30e1327ce4dcae946234a081e
Spencer Young <spencer.young@deliciousai.com>
2024-03-01 12:08:30 -0700

    Made it so model can be saved at different points throughout training (for convergence analysis)

--- deep_uncertainty/experiments/run_experiment.py
+++ deep_uncertainty/experiments/run_experiment.py
@@ -1,4 +1,3 @@
-import os
 from argparse import ArgumentParser
 from argparse import Namespace
 from pathlib import Path
@@ -18,11 +17,14 @@ def main(config: ExperimentConfig):
 
     train_loader, val_loader, test_loader = get_dataloaders(config.dataset_path, config.batch_size)
 
-    for _ in range(config.num_trials):
+    for i in range(config.num_trials):
 
         model = get_model(config)
         checkpoint_callback = ModelCheckpoint(
-            config.chkp_dir / config.experiment_name, every_n_epochs=100
+            dirpath=config.chkp_dir / config.experiment_name / f"version_{i}",
+            every_n_epochs=config.chkp_freq,
+            filename="{epoch}",
+            save_top_k=-1,
         )
         logger = CSVLogger(save_dir=config.log_dir, name=config.experiment_name)
 
@@ -30,7 +32,7 @@ def main(config: ExperimentConfig):
             accelerator=config.accelerator_type.value,
             min_epochs=config.num_epochs,
             max_epochs=config.num_epochs,
-            log_every_n_steps=25,
+            log_every_n_steps=len(train_loader) // 2,
             check_val_every_n_epoch=10,
             enable_model_summary=False,
             callbacks=[checkpoint_callback],
@@ -43,7 +45,6 @@ def main(config: ExperimentConfig):
         log_dir = Path(logger.log_dir)
         config.to_yaml(log_dir / "config.yaml")
         save_losses_plot(log_dir)
-        os.remove(log_dir / "hparams.yaml")  # Unused artifact of lightning.
 
 
 def parse_args() -> Namespace:

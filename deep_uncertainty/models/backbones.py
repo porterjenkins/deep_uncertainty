08b1b158f8ff28914899b6e82d8cef782b5f27ec
Spencer Young <spencermyoung513@gmail.com>
2024-03-07 10:20:53 -0700

    Made it so Poisson predicts the mode instead of the mean at test time. Also added batchnorm to MNIST CNN

--- deep_uncertainty/models/backbones.py
+++ deep_uncertainty/models/backbones.py
@@ -50,7 +50,9 @@ class MNISTCNN(Backbone):
         super(MNISTCNN, self).__init__(output_dim=output_dim)
 
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(64)
         self.flat = nn.Flatten()
         self.fc1 = nn.Linear(5 * 5 * 64, output_dim * 2)
         self.fc2 = nn.Linear(output_dim * 2, output_dim)
@@ -59,8 +61,8 @@ class MNISTCNN(Backbone):
         self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.max_pool(self.relu(self.conv1(x)))
-        x = self.max_pool(self.relu(self.conv2(x)))
+        x = self.bn1(self.max_pool(self.relu(self.conv1(x))))
+        x = self.bn2(self.max_pool(self.relu(self.conv2(x))))
         x = self.dropout(self.flat(x))
         x = self.dropout(self.relu(self.fc1(x)))
         x = self.relu(self.fc2(x))

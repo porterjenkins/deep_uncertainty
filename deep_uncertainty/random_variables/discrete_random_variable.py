bc3941057d781a30abcebd706f3c3fcec83866db
Spencer Young <spencer.young@deliciousai.com>
2024-03-21 14:22:40 -0600

    Updated metric tracking to use calibration metrics we've decided on. Also added code to allow DoublePoisson to be invoked in torch or numpy

--- deep_uncertainty/random_variables/discrete_random_variable.py
+++ deep_uncertainty/random_variables/discrete_random_variable.py
@@ -1,78 +1,145 @@
 import numpy as np
+import torch
 
 
 class DiscreteRandomVariable:
-    """Base class for a discrete random variable, assumed to have support [0, infinity] (truncated at `max_value`)."""
+    """Base class for a discrete random variable, assumed to have support [0, infinity] (truncated at `max_value`).
 
-    def __init__(self, dimension: int, max_value: int = 2000):
+    Attributes:
+        dimension (int): The dimension of the random variable.
+        max_value (int, optional): The value at which this random variable's support is truncated. Defaults to 2000.
+        use_torch (bool, optional): Whether/not to use a pytorch backend for this class's calculations. Defaults to False (numpy).
+    """
+
+    def __init__(self, dimension: int, max_value: int = 2000, use_torch: bool = False):
+        """Initialize a DiscreteRandomVariable.
+
+        Args:
+            dimension (int): The dimension of the random variable.
+            max_value (int, optional): The value at which this random variable's support is truncated. Defaults to 2000.
+            use_torch (bool, optional): Whether/not to use a pytorch backend for this class's calculations. Defaults to False (numpy).
+        """
         self.dimension = dimension
         self.max_value = max_value
+        self.use_torch = use_torch
         self._pmf_vals = None
         self._cdf_vals = None
 
-    def pmf(self, x: int | np.ndarray) -> float | np.ndarray:
+    def pmf(self, x: int | np.ndarray | torch.Tensor) -> float | np.ndarray | torch.Tensor:
+        """Compute the probability of this random variable taking the value(s) x.
+
+        Args:
+            x (int | np.ndarray | torch.Tensor): The value(s) to compute the probability of.
+
+        Returns:
+            float | np.ndarray | torch.Tensor: The probability of x.
+        """
         d = self.pmf_vals.shape[1]
-        result: np.ndarray = self.pmf_vals[x, np.arange(d)]
-        if result.size == 1:
-            return result.item()
+        if not self.use_torch:
+            result = self.pmf_vals[x, np.arange(d)]
+            if result.size == 1:
+                return result.item()
+        else:
+            result = self.pmf_vals[x, torch.arange(d)]
+            if result.numel() == 1:
+                return result.item()
+
         return result
 
-    def ppf(self, q: float) -> int | float | np.ndarray:
+    def ppf(self, q: float | torch.Tensor) -> int | float | np.ndarray | torch.Tensor:
         """Return the smallest possible value of this random variable at which the probability mass to the left is greater than or equal to `q`.
 
         Args:
-            q (float): The desired quantile.
+            q (float | torch.Tensor): The desired quantile.
 
         Returns:
-            int | np.ndarray: The smallest value at which this distribution has mass >= `q` to the left of it.
+            int | np.ndarray | torch.Tensor: The smallest value at which this distribution has mass >= `q` to the left of it.
         """
         mask = self.cdf_vals >= min(q, 0.9999)
-        lowest_val_with_mass_over_q: np.ndarray = np.argmax(mask, axis=0)
-        return (
-            lowest_val_with_mass_over_q.item()
-            if lowest_val_with_mass_over_q.size == 1
-            else lowest_val_with_mass_over_q
-        )
+        if not self.use_torch:
+            lowest_val_with_mass_over_q: np.ndarray = np.argmax(mask, axis=0)
+            if lowest_val_with_mass_over_q.size == 1:
+                return lowest_val_with_mass_over_q.item()
+        else:
+            lowest_val_with_mass_over_q: torch.Tensor = torch.argmax(mask.long(), dim=0)
+            if lowest_val_with_mass_over_q.numel() == 1:
+                return lowest_val_with_mass_over_q.item()
+        return lowest_val_with_mass_over_q
 
-    def cdf(self, x: int | np.ndarray) -> float | np.ndarray:
+    def cdf(self, x: int | np.ndarray | torch.Tensor) -> float | np.ndarray | torch.Tensor:
+        """Compute the probability of this random variable taking a value <= x.
+
+        Args:
+            x (int | np.ndarray | torch.Tensor): The value whose leftward mass will be returned.
+
+        Returns:
+            float | np.ndarray | torch.Tensor: The probability of <= x.
+        """
         d = self.cdf_vals.shape[1]
-        result: np.ndarray = self.cdf_vals[x, np.arange(d)]
-        if result.size == 1:
-            return result.item()
+        if not self.use_torch:
+            result: np.ndarray = self.cdf_vals[x, np.arange(d)]
+            if result.size == 1:
+                return result.item()
+        else:
+            result: torch.Tensor = self.cdf_vals[x, torch.arange(d)]
+            if result.numel() == 1:
+                return result.item()
         return result
 
-    def rvs(self, size: int | tuple) -> np.ndarray:
-        U = np.random.uniform(size=size)
-        draws = np.zeros((U.size, self.dimension))
+    def rvs(self, size: int | tuple) -> np.ndarray | torch.Tensor:
+        """Draw a sample of the given size from this random variable.
+
+        Args:
+            size (int | tuple): The size of the sample to return.
+
+        Returns:
+            np.ndarray | torch.Tensor: The sample from this random variable.
+        """
+        if not self.use_torch:
+            U = np.random.uniform(size=size)
+            draws = np.zeros((U.size, self.dimension))
+        else:
+            U = torch.rand(size)
+            draws = torch.zeros(U.numel(), self.dimension)
+
         for i, u in enumerate(U.ravel()):
             draws[i] = self.ppf(u)
         draws = draws.reshape(size, -1)
         return draws
 
     @property
-    def pmf_vals(self) -> np.ndarray:
+    def pmf_vals(self) -> np.ndarray | torch.Tensor:
         if self._pmf_vals is None:
-            truncated_support = np.arange(self.max_value).reshape(-1, 1)
+            truncated_support = (
+                np.arange(self.max_value).reshape(-1, 1)
+                if not self.use_torch
+                else torch.arange(self.max_value).reshape(-1, 1)
+            )
             self._pmf_vals = self._pmf(truncated_support)
             self._pmf_vals = self._pmf_vals / self._pmf_vals.sum(axis=0)
         return self._pmf_vals
 
     @property
-    def cdf_vals(self) -> np.ndarray:
+    def cdf_vals(self) -> np.ndarray | torch.Tensor:
         if self._cdf_vals is None:
-            self._cdf_vals = np.cumsum(self.pmf_vals, axis=0)
+            self._cdf_vals = (
+                np.cumsum(self.pmf_vals, axis=0)
+                if not self.use_torch
+                else torch.cumsum(self.pmf_vals, dim=0)
+            )
         return self._cdf_vals
 
-    def expected_value(self) -> float | np.ndarray:
+    def expected_value(self) -> float | np.ndarray | torch.Tensor:
+        """Return the expected value of this random variable."""
         raise NotImplementedError("Should be implemented by subclass.")
 
-    def _pmf(self, x: int | np.ndarray) -> float | np.ndarray:
+    def _pmf(self, x: int | np.ndarray | torch.Tensor) -> float | np.ndarray | torch.Tensor:
         """Calculate the probability that this random variable takes on the value(s) x. Does not need to be normalized.
 
         Args:
-            x (int | np.ndarray): The value(s) to compute the (possibly unnormalized) probability of.
+            x (int | np.ndarray | torch.Tensor): The value(s) to compute the (possibly unnormalized) probability of.
 
         Returns:
-            float | np.ndarray: The probability that this distribution takes on the value(s) x.
+            float | np.ndarray | torch.Tensor: The probability that this distribution takes on the value(s) x.
         """
         raise NotImplementedError("Should be implemented by subclass.")

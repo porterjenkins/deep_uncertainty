c342db2d44f8345b14ef1623647d18e5b1a44219
Spencer Young <spencermyoung513@gmail.com>
2024-03-22 15:43:39 -0600

    CUDA fixes for gpu training

--- deep_uncertainty/random_variables/discrete_random_variable.py
+++ deep_uncertainty/random_variables/discrete_random_variable.py
@@ -11,17 +11,27 @@ class DiscreteRandomVariable:
         use_torch (bool, optional): Whether/not to use a pytorch backend for this class's calculations. Defaults to False (numpy).
     """
 
-    def __init__(self, dimension: int, max_value: int = 2000, use_torch: bool = False):
+    def __init__(
+        self,
+        dimension: int,
+        max_value: int = 2000,
+        use_torch: bool = False,
+        device: torch.device | None = None,
+    ):
         """Initialize a DiscreteRandomVariable.
 
         Args:
             dimension (int): The dimension of the random variable.
             max_value (int, optional): The value at which this random variable's support is truncated. Defaults to 2000.
             use_torch (bool, optional): Whether/not to use a pytorch backend for this class's calculations. Defaults to False (numpy).
+            device (torch.device | None, optional): If using pytorch, which device to perform inference on. Defaults to None.
         """
         self.dimension = dimension
         self.max_value = max_value
         self.use_torch = use_torch
+        if use_torch and device is None:
+            raise ValueError("Must specify device to perform inference on if using torch.")
+        self.device = device
         self._pmf_vals = None
         self._cdf_vals = None
 
@@ -40,7 +50,7 @@ class DiscreteRandomVariable:
             if result.size == 1:
                 return result.item()
         else:
-            result = self.pmf_vals[x, torch.arange(d)]
+            result = self.pmf_vals[x, torch.arange(d, device=self.device)]
             if result.numel() == 1:
                 return result.item()
 
@@ -81,7 +91,7 @@ class DiscreteRandomVariable:
             if result.size == 1:
                 return result.item()
         else:
-            result: torch.Tensor = self.cdf_vals[x, torch.arange(d)]
+            result: torch.Tensor = self.cdf_vals[x, torch.arange(d, device=self.device)]
             if result.numel() == 1:
                 return result.item()
         return result
@@ -99,8 +109,8 @@ class DiscreteRandomVariable:
             U = np.random.uniform(size=size)
             draws = np.zeros((U.size, self.dimension))
         else:
-            U = torch.rand(size)
-            draws = torch.zeros(U.numel(), self.dimension)
+            U = torch.rand(size, device=self.device)
+            draws = torch.zeros(U.numel(), self.dimension, device=self.device)
 
         for i, u in enumerate(U.ravel()):
             draws[i] = self.ppf(u)
@@ -113,7 +123,7 @@ class DiscreteRandomVariable:
             truncated_support = (
                 np.arange(self.max_value).reshape(-1, 1)
                 if not self.use_torch
-                else torch.arange(self.max_value).reshape(-1, 1)
+                else torch.arange(self.max_value, device=self.device).reshape(-1, 1)
             )
             self._pmf_vals = self._pmf(truncated_support)
             self._pmf_vals = self._pmf_vals / self._pmf_vals.sum(axis=0)

Added fix for numerical stability issue in NegativeBinomial

deep_uncertainty/models/neg_binom_nn.py
@@ -116,10 +116,11 @@ class NegBinomNN(BaseRegressionNN):
         alpha = alpha.flatten()
 
         # Convert to standard parametrization.
+        eps = torch.tensor(1e-6, device=mu.device)
         var = mu + alpha * mu**2
-        p = mu / var
+        p = mu / torch.maximum(var, eps)
         failure_prob = 1 - p  # Torch docs lie and say this should be P(success).
-        n = mu**2 / (var - mu)
+        n = mu**2 / torch.maximum(var - mu, eps)
         dist = torch.distributions.NegativeBinomial(total_count=n, probs=failure_prob)
         preds = dist.mode
         probs = torch.exp(dist.log_prob(preds))
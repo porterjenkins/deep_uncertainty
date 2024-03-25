928f95f0c29acbcb7fa3ab66fe03d42b5ad17f28
Spencer Young <spencer.young@deliciousai.com>
2024-03-25 11:53:07 -0600

    Added model eval script

---
+++ deep_uncertainty/evaluation/eval_model.py
@@ -0,0 +1,57 @@
+from argparse import ArgumentParser
+from argparse import Namespace
+from pathlib import Path
+
+import lightning as L
+import yaml
+
+from deep_uncertainty.enums import DatasetType
+from deep_uncertainty.experiments.config import ExperimentConfig
+from deep_uncertainty.utils.experiment_utils import get_dataloaders
+from deep_uncertainty.utils.experiment_utils import get_model
+
+
+def main(log_dir: Path, chkp_path: Path):
+
+    config = ExperimentConfig.from_yaml(log_dir / "config.yaml")
+
+    test_loader = get_dataloaders(
+        config.dataset_type,
+        config.dataset_spec,
+        config.batch_size,
+    )[1]
+    if config.dataset_type == DatasetType.TABULAR:
+        input_dim = test_loader.dataset.__getitem__(0)[0].size(-1)
+    else:
+        input_dim = None
+
+    model = get_model(config, input_dim)
+    model.load_from_checkpoint(chkp_path)
+
+    evaluator = L.Trainer(
+        accelerator=config.accelerator_type.value,
+        min_epochs=config.num_epochs,
+        max_epochs=config.num_epochs,
+        enable_model_summary=False,
+    )
+    metrics = evaluator.test(model=model, dataloaders=test_loader)[0]
+    with open(log_dir / "test_metrics.yaml", "w") as f:
+        yaml.dump(metrics, f)
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--log-dir",
+        type=str,
+        help="Directory used to log model metrics/configuration during training.",
+    )
+    parser.add_argument(
+        "--chkp-path", type=str, help="Path to .ckpt where model weights are saved."
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(log_dir=args.log_dir, chkp_path=args.chkp_path)

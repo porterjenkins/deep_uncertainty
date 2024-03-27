d3bd900082a4cfe4c8d41e78f94b4b088e112848
Spencer Young <spencer.young@deliciousai.com>
2024-03-27 14:42:58 -0600

    Added support for double poisson ensemble

---
+++ deep_uncertainty/models/ensembles/double_poisson_mixture_nn.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+import lightning as L
+import torch
+from matplotlib.figure import Figure
+from torchmetrics import MeanAbsoluteError
+from torchmetrics import MeanSquaredError
+from torchmetrics import Metric
+
+from deep_uncertainty.evaluation.custom_torchmetrics import DiscreteExpectedCalibrationError
+from deep_uncertainty.evaluation.custom_torchmetrics import DoublePoissonNLL
+from deep_uncertainty.evaluation.custom_torchmetrics import MedianPrecision
+from deep_uncertainty.experiments.config import EnsembleConfig
+from deep_uncertainty.models import DoublePoissonNN
+from deep_uncertainty.random_variables import DiscreteMixture
+from deep_uncertainty.random_variables import DoublePoisson
+
+
+class DoublePoissonMixtureNN(L.LightningModule):
+    """An ensemble of Double Poisson Neural Nets that outputs a discrete probability distribution over [0, infinity] (truncated at `max_value`).
+
+    The ensemble's predictions are combined as a mixture model with uniform weights.
+    This model is not meant to be trained, and should strictly be used for prediction.
+    """
+
+    def __init__(self, members: Iterable[DoublePoissonNN], max_value: int = 2000):
+        """Initialize a `DoublePoissonMixtureNN`.
+
+        Args:
+            members (Iterable[DoublePoissonNN]): The members of the ensemble.
+            max_value (int, optional): The max value to output probabilities for. Defaults to 2000.
+        """
+        super(DoublePoissonMixtureNN, self).__init__()
+        self.members = members
+        self.max_value = max_value
+        [member.eval() for member in self.members]
+
+        self.rmse = MeanSquaredError(squared=False)
+        self.mae = MeanAbsoluteError()
+        self.discrete_ece = DiscreteExpectedCalibrationError(alpha=2)
+        self.nll = DoublePoissonNLL()
+        self.mp = MedianPrecision()
+
+    def _predict_impl(self, x: torch.Tensor) -> torch.Tensor:
+        """Make a forward pass through the ensemble.
+
+        Args:
+            x (torch.Tensor): Batched input tensor, with shape (N, ...).
+
+        Returns:
+            torch.Tensor: Output probability tensor over [0, `max_value`], with shape (N, `self.max_value`).
+        """
+        dists = []
+        for member in self.members:
+            mu, phi = torch.split(member._predict_impl(x), [1, 1], dim=-1)
+            dists.append(DoublePoisson(mu=mu.flatten(), phi=phi.flatten()))
+
+        mixture = DiscreteMixture(distributions=dists, weights=torch.ones(len(dists)))
+        return mixture.pmf(torch.arange(self.max_value).unsqueeze(1)).transpose(0, 1)
+
+    @property
+    def _test_metrics_dict(self) -> dict[str, Metric]:
+        return {
+            "rmse": self.rmse,
+            "mae": self.mae,
+            "discrete_ece": self.discrete_ece,
+            "nll": self.nll,
+            "mp": self.mp,
+        }
+
+    def _update_test_metrics_batch(self, x: torch.Tensor, y_hat: torch.Tensor, y: torch.Tensor):
+
+        preds = y_hat.argmax(dim=1)
+        probs = y_hat[torch.arange(y_hat.size(0)), preds]
+        targets = y.flatten()
+
+        mu = (y_hat * torch.arange(self.max_value)).sum(dim=1)
+        var = (y_hat * (torch.arange(self.max_value).unsqueeze(1) - mu).transpose(0, 1) ** 2).sum(
+            dim=1
+        )
+        precision = 1 / var
+
+        self.discrete_ece.update(
+            preds=preds,
+            probs=probs,
+            targets=targets,
+        )
+        self.rmse.update(preds, targets)
+        self.mae.update(preds, targets)
+        self.nll.update(mu=mu, phi=mu / var, targets=targets)
+        self.mp.update(precision)
+
+    def test_step(self, batch: torch.Tensor) -> torch.Tensor:
+        x, y = batch
+        y_hat = self._predict_impl(x)
+        self._update_test_metrics_batch(x, y_hat, y.view(-1, 1).float())
+        return y_hat
+
+    def predict_step(self, batch: torch.Tensor) -> torch.Tensor:
+        x, _ = batch
+        y_hat = self._predict_impl(x)
+        return y_hat
+
+    def on_test_epoch_end(self):
+        for name, metric_tracker in self._test_metrics_dict.items():
+            self.log(name, metric_tracker.compute())
+            if name in {"mean_calibration", "mp"}:
+                fig: Figure = metric_tracker.plot()
+                if self.logger is not None:
+                    root = self.logger.log_dir or "."
+                else:
+                    root = "."
+                fig.savefig(root + f"/{name}_plot.png")
+
+    @staticmethod
+    def from_config(config: EnsembleConfig) -> DoublePoissonMixtureNN:
+        """Construct a DoublePoissonMixtureNN from a config. This is the primary way of building an ensemble.
+
+        Args:
+            config (EnsembleConfig): Ensemble config object.
+
+        Returns:
+            DoublePoissonMixtureNN: The specified ensemble of DoublePoissonNN models.
+        """
+        # TODO: Support for changing max value?
+        checkpoint_paths = config.members
+        members = []
+        for path in checkpoint_paths:
+            member = DoublePoissonNN.load_from_checkpoint(path)
+            members.append(member)
+        return DoublePoissonMixtureNN(members=members)

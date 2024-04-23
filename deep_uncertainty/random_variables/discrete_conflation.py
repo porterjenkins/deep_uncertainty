1d7eee5d75e14cbc75b3328c1189041df2a864cb
Spencer Young <spencer.young@deliciousai.com>
2024-04-23 13:18:31 -0600

    Added new toy count dataset, updated discrete conflation code

--- deep_uncertainty/random_variables/discrete_conflation.py
+++ deep_uncertainty/random_variables/discrete_conflation.py
@@ -1,8 +1,11 @@
 import numpy as np
-from scipy.stats import rv_discrete
+import torch
+from torch.distributions import Distribution
+
+from deep_uncertainty.random_variables.discrete_random_variable import DiscreteRandomVariable
 
 
-class DiscreteConflation:
+class DiscreteConflation(DiscreteRandomVariable):
     """A conflation of discrete random variables, as described in https://www.researchgate.net/publication/1747728_Conflations_of_Probability_Distributions.
 
     Given discrete random variables X_1, X_2, ..., X_n, with pmfs g_{X_1}, g_{X_2}, ..., g_{X_n}, the conflation Q = X_1 & X_2 & ... & X_n has pmf given by
@@ -12,95 +15,41 @@ class DiscreteConflation:
     where Z is a normalizing constant so the masses sum to 1. This is a proper random variable with certain desirable properties for combining distributions.
 
     Args:
-        random_variables (list(DiscreteRandomVariables)): A list of the discrete random variables to conflate (i.e. X_1, X_2, ..., X_n)
-        support (np.ndarray): An array describing the support of the conflation (should be discrete). This should be the intersection of the support of X_1, ..., X_n.
+        random_variables (list[Distribution | DiscreteRandomVariable]): A list of the discrete random variables to conflate (i.e. X_1, X_2, ..., X_n)
     """
 
     def __init__(
         self,
-        random_variables: list[rv_discrete],
-        support: np.ndarray,
+        random_variables: list[Distribution | DiscreteRandomVariable],
     ) -> None:
-        self.random_variables = random_variables
-        self.support = support
-        self._fit()
-
-    def _fit(self) -> None:
-        """Precompute the pmf of this discrete conflation over the entire support."""
-        running_product = np.ones_like(self.support, dtype=float)
-        for rv in self.random_variables:
-            running_product *= rv.pmf(self.support).squeeze()
-
-        self.unnormalized_mass = running_product
-        self.Z = running_product.sum()
-        self.mass = self.unnormalized_mass / self.Z
-
-    def pmf(self, x: int | np.ndarray) -> float | np.ndarray:
-        """Calculate the probability that this conflation takes on the value(s) x.
-
-        Args:
-            x (int | np.ndarray): The value(s) to compute the probability of.
-
-        Raises:
-            ValueError: If x is not in the support of the conflation.
-
-        Returns:
-            probability (float | np.ndarray): The probability of x.
-        """
-        if not np.all(np.in1d(x, self.support)):
-            raise ValueError(f"Provided value(s) {x} not in the support of this random variable.")
-
-        if isinstance(x, np.ndarray):
-            indices = [np.where(self.support == val)[0][0] for val in x if val in self.support]
-            probability = self.mass[indices]
-
+        if isinstance(random_variables[0], Distribution):
+            dimension = random_variables[0].mean.numel()
+            use_torch = True
+            device = random_variables[0].mean.device
         else:
-            probability = self.mass[np.where(self.support == x)][0]
+            dimension = random_variables[0].dimension
+            use_torch = random_variables[0].use_torch
+            device = random_variables[0].device
+        super().__init__(dimension=dimension, use_torch=use_torch, device=device)
+        self.random_variables = random_variables
 
-        return probability
+    def _pmf(self, x: int | np.ndarray | torch.Tensor) -> float | np.ndarray | torch.Tensor:
+        pmf_vals = []
+        for rv in self.random_variables:
+            if isinstance(rv, DiscreteRandomVariable):
+                pmf_vals.append(rv.pmf(x))
+            else:
+                pmf_vals.append(torch.exp(rv.log_prob(x)))
+        if self.use_torch:
+            return torch.cat(pmf_vals, dim=1).prod(dim=1, keepdim=True)
+        else:
+            return np.concatenate(pmf_vals, axis=1).prod(axis=1, keepdims=True)
 
-    def logpmf(self, x: int | np.ndarray) -> float | np.ndarray:
-        """Calculate the log probability that this conflation takes on the value(s) x.
 
-        Args:
-            x (int | np.ndarray): The value(s) to compute the log probability of.
+if __name__ == "__main__":
+    from torch.distributions import Poisson
 
-        Raises:
-            ValueError: If x is not in the support of the conflation.
-
-        Returns:
-            log_probability (float | np.ndarray): The log probability of x.
-        """
-        if not np.all(np.in1d(x, self.support)):
-            raise ValueError(f"Provided value(s) {x} not in the support of this random variable.")
-
-        log_probability = np.log(self.pmf(x))
-        return log_probability
-
-    def nll(self, x: int | np.ndarray) -> float | np.ndarray:
-        """Calculate the negative log likelihood of x for this conflation.
-
-        Args:
-            x (int | np.ndarray): The value(s) to compute the negative log likelihood of.
-
-        Returns:
-            nll (float | np.ndarray): The negative log likelihood of x.
-        """
-        nll = -self.logpmf(x)
-        return nll
-
-    def ppf(self, q: float) -> int | np.ndarray:
-        """Return the smallest possible value of this conflation at which the probability mass to the left is greater than or equal to `q`.
-
-        Args:
-            q (float): The desired quantile.
-
-        Returns:
-            int | np.ndarray: The smallest value at which this distribution has mass >= `q` to the left of it.
-        """
-        mass = self.pmf(self.support.reshape(-1, 1)).reshape(-1, 1)
-        mass = mass / mass.sum(axis=0)  # Sometimes, the resultant mass isn't entirely normalized.
-        mask = np.cumsum(mass, axis=0) >= min(q, 0.9999)
-        indices = np.argmax(mask, axis=0)
-        values = self.support[indices]
-        return values.item() if values.size == 1 else values
+    rv_list = [Poisson(rate=4), Poisson(rate=7)]
+    conflation = DiscreteConflation(rv_list)
+    support = torch.arange(15)
+    conflation.pmf(support)

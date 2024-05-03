0b4dd4e22cb79e73003a1cc472ba113c6d389c73
Longchao Da <longchao@en4228496l.fulton.ad.asu.edu>
2024-05-02 17:34:22 -0700

    update gitignore

---
+++ deep_uncertainty/test_read.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# data = np.load("./sales.npz")
+data = np.load("./prescriptions.npz")
+
+print(data)
---
+++ examine.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+data = np.load("sales.npz")
+
+train_data = data['X_train']
+test_data = data['X_test']
+
+for 
+
+print("a")

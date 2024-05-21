c69d785600687899869bb3b97981851a7f30a063
Spencer Young <spencer.young@deliciousai.com>
2024-05-21 10:00:12 -0600

    More figure updates

--- deep_uncertainty/random_variables/__init__.py
+++ deep_uncertainty/random_variables/__init__.py
@@ -1,5 +1,3 @@
 from .discrete_conflation import DiscreteConflation
 from .discrete_mixture import DiscreteMixture
-from .discrete_truncated_normal import DiscreteTruncatedNormal
 from .double_poisson import DoublePoisson
-from .gamma_count import GammaCount

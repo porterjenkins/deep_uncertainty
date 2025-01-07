Numerical stability fix for nbinom

deep_uncertainty/models/neg_binom_nn.py
@@ -127,17 +127,19 @@ class NegBinomNN(DiscreteRegressionNN):
         Returns:
             torch.distributions.NegativeBinomial: The implied negative binomial distribution over y.
         """
+        eps = torch.tensor(1e-3, device=y_hat.device)
+
         mu, alpha = torch.split(y_hat, [1, 1], dim=-1)
         mu = mu.flatten()
-        alpha = alpha.flatten()
+        alpha = torch.clamp(alpha, min=eps).flatten()
 
         # Convert to standard parametrization.
-        eps = torch.tensor(1e-6, device=mu.device)
         var = mu + alpha * mu**2
         p = mu / torch.maximum(var, eps)
-        failure_prob = torch.minimum(
-            1 - p, 1 - eps
-        )  # Torch docs lie and say this should be P(success).
+
+        # Torch docs lie and say this should be P(success).
+        failure_prob = torch.clamp(1 - p, min=eps, max=1 - eps)
+
         n = mu**2 / torch.maximum(var - mu, eps)
         dist = torch.distributions.NegativeBinomial(total_count=n, probs=failure_prob)
         return dist